56412e32e4cc48a98b2c5dae0c33492ae7f471fe
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-16 15:48:21 -0300

    feat: create SwitchCaseForm component to show the right form component step

---
+++ src/screens/Register/SwitchCaseForm/styles.ts
@@ -0,0 +1,22 @@
+import { moderateScale } from 'react-native-size-matters';
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const StepNumber = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    font-size: ${theme.fontSize.xxlarge}px;
+    font-family: ${theme.fonts.bold};
+  `};
+`;
+
+export const StepDescription = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.text400};
+    font-size: ${theme.fontSize.large}px;
+    font-family: ${theme.fonts.bold};
+    margin: ${moderateScale(2)}px 0 ${moderateScale(16)}px 0;
+    text-transform: uppercase;
+  `};
+`;
