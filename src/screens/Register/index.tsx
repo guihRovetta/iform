8fbe0d023df34286bfbc7ac76f70baf6fd5577c9
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-16 15:13:04 -0300

    feat: create Register screen

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import PageLayout from '../../components/PageLayout';
+
+const Register = () => {
+  return (
+    <PageLayout
+      title="Registrar"
+      subtitle="Bem-vindo, selecione uma das opções abaixo para prosseguir"
+    >
+      <Text>aquii</Text>
+    </PageLayout>
+  );
+};
+
+export default Register;
