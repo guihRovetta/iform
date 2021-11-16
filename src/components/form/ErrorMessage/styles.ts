85a84c8b89bb6def7c8ad8485b3d8c57e43b707a
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-15 21:43:09 -0300

    fix: adjust style details

--- src/components/form/ErrorMessage/styles.ts
+++ src/components/form/ErrorMessage/styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components/native';
 
 export const StyledErrorMessage = styled.Text`
   ${({ theme }) => css`
-    margin-top: ${moderateScale(4)}px;
+    margin-top: ${moderateScale(2)}px;
     margin-left: ${moderateScale(4)}px;
     color: ${theme.colors.red500};
     font-size: ${theme.fontSize.xregular}px;
--- src/components/form/Label/styles.ts
+++ src/components/form/Label/styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components/native';
 
 export const StyledLabel = styled.Text`
   ${({ theme }) => css`
-    margin-bottom: ${moderateScale(4)}px;
+    margin-bottom: ${moderateScale(2)}px;
     margin-left: ${moderateScale(4)}px;
     color: ${theme.colors.text500};
     font-size: ${theme.fontSize.xregular}px;
