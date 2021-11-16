b2195e7a59e39b6fe846830a9f88f761b1226d7b
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-15 21:07:45 -0300

    feat: create MaskedInput component

---
+++ src/components/form/MaskedInput/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { MaskInputProps } from 'react-native-mask-input';
+import { useTheme } from 'styled-components';
+
+import ErrorMessage from '../ErrorMessage';
+import { CheckCircleIcon, XCircleIcon } from '../FormFieldStatusIcons/styles';
+import Label from '../Label';
+import { Container, StyledInput } from './styles';
+
+type Props = MaskInputProps & {
+  label: string;
+  touched?: boolean;
+  error?: string;
+};
+
+const MaskedInput = ({ label, touched = false, error, ...rest }: Props) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Label>{label}</Label>
+
+      <Container
+        status={
+          touched && error ? 'error' : touched && !error ? 'success' : 'normal'
+        }
+      >
+        <StyledInput {...rest} selectionColor={theme.colors.primary} />
+
+        {touched && error && <XCircleIcon name="x-circle" />}
+        {touched && !error && <CheckCircleIcon name="check-circle" />}
+      </Container>
+
+      {touched && error && <ErrorMessage>{error}</ErrorMessage>}
+    </>
+  );
+};
+
+export default MaskedInput;
---
+++ src/components/form/MaskedInput/styles.ts
@@ -0,0 +1,31 @@
+import MaskInput from 'react-native-mask-input';
+import { moderateScale } from 'react-native-size-matters';
+import styled, { css } from 'styled-components/native';
+
+import {
+  formFieldStatusBorderModifiers,
+  FormFieldStatusBorderProps,
+} from '../FormFieldStatusBorder/styles';
+
+export const Container = styled.View<FormFieldStatusBorderProps>`
+  ${({ theme, status }) => css`
+    background-color: ${theme.colors.grey100};
+    border-radius: ${moderateScale(8)}px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: ${moderateScale(16)}px;
+
+    ${!!status && formFieldStatusBorderModifiers[status](theme)}
+  `}
+`;
+
+export const StyledInput = styled(MaskInput)`
+  ${({ theme }) => css`
+    color: ${theme.colors.text500};
+    font-family: ${theme.fonts.regular};
+    flex: 1;
+  `}
+`;
+
+export const ShowPasswordButton = styled.TouchableOpacity``;
