7787452e9cf93bcf8dfa37eab1f5a5778d0e2554
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-17 11:48:14 -0300

    feat: create form helpers (like: messages and regex)

---
+++ src/utils/formHelpers.ts
@@ -0,0 +1,8 @@
+// MESSAGES
+export const REQUIRED_FIELD_MESSAGE = 'O campo é obrigatório';
+export const MIN_CHAR_MESSAGE = 'O campo deve conter no mínimo';
+export const MAX_CHAR_MESSAGE = 'O campo deve conter no máximo';
+export const VALID_FIELD_MESSAGE = 'Informe um valor válido para o campo';
+
+// REGEX
+export const NAME_REGEX = /^[A-Za-z ]*$/;
