d7e9f1a2007bfac2e6de8bb0f85c1567f09d027e
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-18 12:16:09 -0300

    feat: add missing form fields to BasicInfoForm schema

--- src/screens/Register/BasicInfoForm/schema.ts
+++ src/screens/Register/BasicInfoForm/schema.ts
@@ -1,6 +1,10 @@
+import { sub } from 'date-fns';
 import * as yup from 'yup';
 
 import {
+  EIGHTEEN_YEARS_DATE_MESSAGE,
+  HUNDRED_YEARS_DATE_MESSAGE,
+  INVALID_FORM_MESSAGE,
   MAX_CHAR_MESSAGE,
   MIN_CHAR_MESSAGE,
   NAME_REGEX,
@@ -8,10 +12,15 @@ import {
   VALID_FIELD_MESSAGE,
 } from '../../../utils/formHelpers';
 
+const TODAY_DATE = new Date();
+const EIGHTEEN_YEARS_DATE = sub(TODAY_DATE, { years: 18 });
+const HUNDRED_YEARS_DATE = sub(TODAY_DATE, { years: 100 });
+
 export const schema = yup
   .object({
     name: yup
       .string()
+      .typeError(INVALID_FORM_MESSAGE)
       .required(REQUIRED_FIELD_MESSAGE)
       .matches(NAME_REGEX, VALID_FIELD_MESSAGE)
       .min(3, ({ min }) => {
@@ -20,5 +29,35 @@ export const schema = yup
       .max(25, ({ max }) => {
         return `${MAX_CHAR_MESSAGE} ${max} caracteres`;
       }),
+    dateOfBirth: yup
+      .date()
+      .typeError(INVALID_FORM_MESSAGE)
+      .required(REQUIRED_FIELD_MESSAGE)
+      .max(EIGHTEEN_YEARS_DATE, EIGHTEEN_YEARS_DATE_MESSAGE)
+      .min(HUNDRED_YEARS_DATE, HUNDRED_YEARS_DATE_MESSAGE),
+    cellphone: yup
+      .string()
+      .typeError(INVALID_FORM_MESSAGE)
+      .required(REQUIRED_FIELD_MESSAGE)
+      .length(11, VALID_FIELD_MESSAGE),
+    cpf: yup
+      .string()
+      .typeError(INVALID_FORM_MESSAGE)
+      .required(REQUIRED_FIELD_MESSAGE)
+      .length(11, VALID_FIELD_MESSAGE),
+    maritalStatus: yup
+      .string()
+      .typeError(INVALID_FORM_MESSAGE)
+      .required(REQUIRED_FIELD_MESSAGE),
+    avatar: yup.object().shape({
+      uri: yup
+        .string()
+        .typeError(INVALID_FORM_MESSAGE)
+        .required(REQUIRED_FIELD_MESSAGE),
+      base64: yup
+        .string()
+        .typeError(INVALID_FORM_MESSAGE)
+        .required(REQUIRED_FIELD_MESSAGE),
+    }),
   })
   .required();
