b3386580cc08a541116b8f680c6b35b8001aad15
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-11-18 12:15:37 -0300

    feat: add form field error messages to formHelpers

--- src/utils/formHelpers.ts
+++ src/utils/formHelpers.ts
@@ -1,8 +1,13 @@
 // MESSAGES
+export const INVALID_FORM_MESSAGE = 'Formato inválido';
 export const REQUIRED_FIELD_MESSAGE = 'O campo é obrigatório';
 export const MIN_CHAR_MESSAGE = 'O campo deve conter no mínimo';
 export const MAX_CHAR_MESSAGE = 'O campo deve conter no máximo';
 export const VALID_FIELD_MESSAGE = 'Informe um valor válido para o campo';
+export const HUNDRED_YEARS_DATE_MESSAGE =
+  'O usuário não pode ter mais que 100 anos';
+export const EIGHTEEN_YEARS_DATE_MESSAGE =
+  'O usuário deve ter no mínimo 18 anos';
 
 // REGEX
 export const NAME_REGEX = /^[A-Za-z ]*$/;
