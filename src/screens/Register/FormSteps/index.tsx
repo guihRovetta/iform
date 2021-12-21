726896a5ef1aac71ebcbc834227e4216c38d957a
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-12-21 13:26:00 -0300

    refactor: refactor component to use objects instead switch case, rename component

---
+++ src/screens/Register/FormSteps/index.tsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+
+import AddressForm from '../AddressForm';
+import BasicInfoForm from '../BasicInfoForm';
+import RegisterFormContext from '../context/RegisterForm';
+import { Container, StepNumber, StepDescription } from './styles';
+
+const FormSteps = () => {
+  const { step } = useContext(RegisterFormContext);
+
+  const formSteps = {
+    0: {
+      title: 'Informações pessoais',
+      component: <BasicInfoForm />,
+    },
+    1: {
+      title: 'Endereço',
+      component: <AddressForm />,
+    },
+    2: {
+      title: 'Conta',
+      component: null,
+    },
+    3: {
+      title: 'Confirmação',
+      component: null,
+    },
+  };
+
+  return (
+    <Container>
+      <StepNumber>{step + 1}.</StepNumber>
+      <StepDescription>{formSteps[step]?.title}</StepDescription>
+      {formSteps[step]?.component}
+    </Container>
+  );
+};
+
+export default FormSteps;
