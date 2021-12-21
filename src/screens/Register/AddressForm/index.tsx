2d5a7f3803dcd5e161e7f2a2d454855b51ce59af
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-12-21 15:39:38 -0300

    feat: make getAddress data integration to AddressForm component

--- src/screens/Register/AddressForm/index.tsx
+++ src/screens/Register/AddressForm/index.tsx
@@ -1,16 +1,19 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Controller, useFieldArray, useForm } from 'react-hook-form';
+import { ActivityIndicator, Alert } from 'react-native';
 import { Masks } from 'react-native-mask-input';
+import { useTheme } from 'styled-components/native';
 
 import Button from '../../../components/Button';
 import FormFieldWrapper from '../../../components/Form/FormFieldWrapper';
 import Input from '../../../components/Form/Input';
 import MaskedInput from '../../../components/Form/MaskedInput';
 import FormLayout from '../../../components/FormLayout';
+import { getAddress } from '../../../services/api/address';
 import RegisterFormContext from '../context/RegisterForm';
 import { schema } from './schema';
-import { ActionWrapper } from './styles';
+import { ActionWrapper, Title } from './styles';
 
 type FormProps = {
   address: {
@@ -39,14 +42,18 @@ const defaultValues = {
 };
 
 const AdressForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { step, setStep } = useContext(RegisterFormContext);
+
+  const { colors } = useTheme();
 
   const {
     control,
     formState: { errors, touchedFields, isDirty, isValid },
-    getValues,
-    setValue,
     watch,
+    setValue,
+    setError,
   } = useForm<FormProps>({
     resolver: yupResolver(schema),
     defaultValues,
@@ -75,10 +82,36 @@ const AdressForm = () => {
     }
   };
 
+  const handleChangeZipcode = async (zipcode: string, index: number) => {
+    if (zipcode?.length !== 8) return;
+
+    try {
+      setIsLoading(true);
+      const address = await getAddress(zipcode);
+      setValue(`address.${index}`, address);
+      if (!address?.city || !address?.state) {
+        setZipcodeFormFieldError(index);
+      }
+    } catch (error) {
+      Alert.alert('😔 Ops...', error?.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const setZipcodeFormFieldError = (index: number) => {
+    setError(`address.${index}.zipcode`, {
+      type: 'invalid',
+      message: 'Informe um CEP válido',
+    });
+  };
+
   return (
     <FormLayout>
       {fields?.map((field, index) => (
         <React.Fragment key={field?.id}>
+          <Title>{`${index + 1}. Endereço`}</Title>
+
           <Controller
             control={control}
             render={({ field: { onChange, onBlur, value } }) => (
@@ -87,7 +120,10 @@ const AdressForm = () => {
                   label="CEP"
                   placeholder="Digite seu CEP"
                   onBlur={onBlur}
-                  onChangeText={(_, unmaskedValue) => onChange(unmaskedValue)}
+                  onChangeText={(_, unmaskedValue) => {
+                    onChange(unmaskedValue);
+                    handleChangeZipcode(unmaskedValue, index);
+                  }}
                   value={value}
                   error={errors?.address?.[index]?.zipcode?.message}
                   touched={touchedFields?.address?.[index]?.zipcode}
@@ -99,112 +135,119 @@ const AdressForm = () => {
             name={`address.${index}.zipcode`}
           />
 
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <FormFieldWrapper>
-                <Input
-                  label="Rua"
-                  onBlur={onBlur}
-                  onChangeText={(newValue) => onChange(newValue)}
-                  value={value}
-                  error={errors?.address?.[index]?.street?.message}
-                  touched={touchedFields?.address?.[index]?.street}
-                  disabled
-                />
-              </FormFieldWrapper>
-            )}
-            name={`address.${index}.street`}
-          />
+          {isLoading && <ActivityIndicator color={colors?.primary} />}
 
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <FormFieldWrapper>
-                <Input
-                  label="Número"
-                  onBlur={onBlur}
-                  onChangeText={(newValue) => onChange(newValue)}
-                  value={value}
-                  error={errors?.address?.[index]?.number?.message}
-                  touched={touchedFields?.address?.[index]?.number}
-                  keyboardType="number-pad"
+          {arrayWatchedField?.[index]?.city &&
+            arrayWatchedField?.[index]?.state && (
+              <>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <FormFieldWrapper>
+                      <Input
+                        label="Rua"
+                        onBlur={onBlur}
+                        onChangeText={(newValue) => onChange(newValue)}
+                        value={value}
+                        error={errors?.address?.[index]?.street?.message}
+                        touched={touchedFields?.address?.[index]?.street}
+                        disabled
+                      />
+                    </FormFieldWrapper>
+                  )}
+                  name={`address.${index}.street`}
                 />
-              </FormFieldWrapper>
-            )}
-            name={`address.${index}.number`}
-          />
 
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <FormFieldWrapper>
-                <Input
-                  label="Complemento (opcional)"
-                  onBlur={onBlur}
-                  onChangeText={(newValue) => onChange(newValue)}
-                  value={value}
-                  error={errors?.address?.[index]?.complement?.message}
-                  touched={touchedFields?.address?.[index]?.complement}
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <FormFieldWrapper>
+                      <Input
+                        label="Número"
+                        onBlur={onBlur}
+                        onChangeText={(newValue) => onChange(newValue)}
+                        value={value}
+                        error={errors?.address?.[index]?.number?.message}
+                        touched={touchedFields?.address?.[index]?.number}
+                        keyboardType="number-pad"
+                      />
+                    </FormFieldWrapper>
+                  )}
+                  name={`address.${index}.number`}
                 />
-              </FormFieldWrapper>
-            )}
-            name={`address.${index}.complement`}
-          />
 
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <FormFieldWrapper>
-                <Input
-                  label="Bairro"
-                  onBlur={onBlur}
-                  onChangeText={(newValue) => onChange(newValue)}
-                  value={value}
-                  error={errors?.address?.[index]?.neighborhood?.message}
-                  touched={touchedFields?.address?.[index]?.neighborhood}
-                  disabled
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <FormFieldWrapper>
+                      <Input
+                        label="Complemento (opcional)"
+                        onBlur={onBlur}
+                        onChangeText={(newValue) => onChange(newValue)}
+                        value={value}
+                        error={errors?.address?.[index]?.complement?.message}
+                        touched={touchedFields?.address?.[index]?.complement}
+                      />
+                    </FormFieldWrapper>
+                  )}
+                  name={`address.${index}.complement`}
                 />
-              </FormFieldWrapper>
-            )}
-            name={`address.${index}.neighborhood`}
-          />
 
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <FormFieldWrapper>
-                <Input
-                  label="Cidade"
-                  onBlur={onBlur}
-                  onChangeText={(newValue) => onChange(newValue)}
-                  value={value}
-                  error={errors?.address?.[index]?.city?.message}
-                  touched={touchedFields?.address?.[index]?.city}
-                  disabled
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <FormFieldWrapper>
+                      <Input
+                        label="Bairro"
+                        onBlur={onBlur}
+                        onChangeText={(newValue) => onChange(newValue)}
+                        value={value}
+                        error={errors?.address?.[index]?.neighborhood?.message}
+                        touched={touchedFields?.address?.[index]?.neighborhood}
+                        disabled
+                      />
+                    </FormFieldWrapper>
+                  )}
+                  name={`address.${index}.neighborhood`}
                 />
-              </FormFieldWrapper>
-            )}
-            name={`address.${index}.city`}
-          />
 
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <FormFieldWrapper>
-                <Input
-                  label="Estado"
-                  onBlur={onBlur}
-                  onChangeText={(newValue) => onChange(newValue)}
-                  value={value}
-                  error={errors?.address?.[index]?.state?.message}
-                  touched={touchedFields?.address?.[index]?.state}
-                  disabled
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <FormFieldWrapper>
+                      <Input
+                        label="Cidade"
+                        onBlur={onBlur}
+                        onChangeText={(newValue) => onChange(newValue)}
+                        value={value}
+                        error={errors?.address?.[index]?.city?.message}
+                        touched={touchedFields?.address?.[index]?.city}
+                        disabled
+                      />
+                    </FormFieldWrapper>
+                  )}
+                  name={`address.${index}.city`}
                 />
-              </FormFieldWrapper>
+
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <FormFieldWrapper>
+                      <Input
+                        label="Estado"
+                        onBlur={onBlur}
+                        onChangeText={(newValue) => onChange(newValue)}
+                        value={value}
+                        error={errors?.address?.[index]?.state?.message}
+                        touched={touchedFields?.address?.[index]?.state}
+                        disabled
+                      />
+                    </FormFieldWrapper>
+                  )}
+                  name={`address.${index}.state`}
+                />
+              </>
             )}
-            name={`address.${index}.state`}
-          />
 
           <ActionWrapper>
             <Button
--- src/screens/Register/AddressForm/styles.ts
+++ src/screens/Register/AddressForm/styles.ts
@@ -1,3 +1,4 @@
+import { moderateScale } from 'react-native-size-matters';
 import styled, { css } from 'styled-components/native';
 
 export const ActionWrapper = styled.View`
@@ -6,5 +7,15 @@ export const ActionWrapper = styled.View`
     align-items: center;
     justify-content: space-between;
     margin-bottom: ${theme.spacing.small}px;
+  `};
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.primary};
+    font-size: ${theme.fontSize.large}px;
+    font-family: ${theme.fonts.bold};
+    margin: ${moderateScale(2)}px 0 ${moderateScale(16)}px 0;
+    align-self: center;
   `};
 `;
