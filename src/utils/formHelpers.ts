6ec3e91be7f8b039ea027a0bb373c33857bebae5
Guilherme Rovetta <guilherme020213@hotmail.com>
2021-12-22 15:22:50 -0300

    feat: add messages to formHelpers

--- src/utils/formHelpers.ts
+++ src/utils/formHelpers.ts
@@ -8,6 +8,10 @@ export const HUNDRED_YEARS_DATE_MESSAGE =
   'O usuário não pode ter mais que 100 anos';
 export const EIGHTEEN_YEARS_DATE_MESSAGE =
   'O usuário deve ter no mínimo 18 anos';
+export const EMAIL_FIELD_MESSAGE = 'Informe um email válido';
+export const PASSWORD_FIELD_MESSAGE = 'A senha deve conter apenas letras';
+export const CONFIRM_PASSWORD_FIELD_MESSAGE = 'As senhas devem ser as mesmas';
 
 // REGEX
 export const NAME_REGEX = /^[A-Za-z ]*$/;
+export const PASSWORD_REGEX = /^[A-Za-z ]*$/;
