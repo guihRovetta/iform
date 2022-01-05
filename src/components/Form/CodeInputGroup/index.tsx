d2b010b18bbf2ebf069f5000087a5df85ecdc997
Guilherme Rovetta <guilherme020213@hotmail.com>
2022-01-05 10:10:48 -0300

    feat: create CodeInputGroup component

---
+++ src/components/Form/CodeInputGroup/index.tsx
@@ -0,0 +1,55 @@
+import React, { createRef, useRef, useState } from 'react';
+import { TextInput } from 'react-native';
+
+import FormFieldWrapper from '../FormFieldWrapper';
+import Input from '../Input';
+import { Container, InputWrapper } from './styles';
+
+type Props = {
+  length: number;
+  onChangeValue: (inputArray: string[]) => void;
+};
+
+const CodeInputGroup = ({ length, onChangeValue }: Props) => {
+  const generatedArray = Array.from({ length }, (_) => '');
+  const inputRefsArray = useRef<React.RefObject<TextInput>[]>(
+    generatedArray?.map(() => createRef())
+  );
+
+  const [codeGroup, setCodeGroup] = useState<string[]>(generatedArray);
+
+  const handleChangeText = (inputValue: string, index: number) => {
+    const newArray = [...codeGroup];
+    newArray?.splice(index, 1, inputValue);
+    setCodeGroup(newArray);
+    onChangeValue(newArray);
+    if (inputValue) handleFocusNextInput(index);
+  };
+
+  const handleFocusNextInput = (index: number) => {
+    const nextInputIndex = index + 1;
+    if (nextInputIndex < length) {
+      inputRefsArray?.current[index + 1]?.current?.focus();
+    }
+  };
+
+  return (
+    <FormFieldWrapper>
+      <Container>
+        {generatedArray?.map((_, index) => (
+          <InputWrapper key={`code-input-${index}`}>
+            <Input
+              onChangeText={(inputValue) => handleChangeText(inputValue, index)}
+              keyboardType="number-pad"
+              maxLength={1}
+              ref={inputRefsArray?.current[index]}
+              textAlign="center"
+            />
+          </InputWrapper>
+        ))}
+      </Container>
+    </FormFieldWrapper>
+  );
+};
+
+export default CodeInputGroup;
---
+++ src/components/Form/CodeInputGroup/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`;
+
+export const InputWrapper = styled.View`
+  flex: 1;
+  min-width: 60px;
+  max-width: 64px;
+`;
